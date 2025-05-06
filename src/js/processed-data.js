a6bf1d452ae9841111ae09ddc8fa3d57a4de2e87
这里输入你的GitHub用户名 <pawazwz@gmail.com>
2025-05-06 12:23:58 +0800

    Fix Vercel build

--- src/js/processed-data.js
+++ src/js/processed-data.js
@@ -1,6 +1,6 @@
 /**
  * 预处理后的ETF数据
- * 自动生成于 2025/5/6 12:05:22
+ * 自动生成于 2025/5/6 12:14:35
  * 请勿手动修改此文件
  */
 
@@ -3156,7 +3156,7 @@ const processedData = {
       "formattedDate": "2022-04-22"
     }
   },
-  "lastUpdated": "2025-05-06T04:05:22.264Z",
+  "lastUpdated": "2025-05-06T04:14:35.786Z",
   "summary": {
     "totalUnit": 150,
     "totalProfit": 46.202,
